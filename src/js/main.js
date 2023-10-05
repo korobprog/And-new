1ff37610fd832f727397566c17d5ac93b2cf071f
korobprog <korobprog@gmail.com>
2023-10-05 20:07:21 +0300

    form add

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,21 @@
 import '../assets/scss/main.scss'
+
+const lists = document.querySelectorAll('.list');
+
+function addTask() {
+   const btn = document.querySelector('.add_btn');
+   const addBtn = document.querySelector('.add_item-btn');
+   const cancelBtn = document.querySelector('.cancel_item-btn');
+   const textarea = document.querySelector('.textarea');
+   const form = document.querySelector('.form');
+
+   let value
+
+   btn.addEventListener('click', () => {
+      form.style.display = 'block'
+      btn.style.display = 'none'
+      addBtn.style.display = 'none'
+   })
+}
+
+addTask();
